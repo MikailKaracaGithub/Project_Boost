f21c43ee03211878ac920ebb8df426018540d0c1
Xavaltir <tombis1999@hotmail.com>
2019-08-27 01:42:52 +0200

    Added movement Script not finished tho

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+        }
+        if (Input.GetKey(KeyCode.A)){
+            print("Rotating Left");
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
